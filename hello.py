b9528d61cd232c11f7d0cf56830396f55c0bcb55
torcuata22 <marilynjmarquez@gmail.com>
2023-04-11 14:15:52 -0500

    boot

--- hello.py
+++ hello.py
@@ -1,26 +1,19 @@
 from flask import Flask, render_template
 
 #create flask instance:
+
+ 
 app = Flask(__name__)
-
-
-'''
-FILTERS:
-safe --> to pass html (stop jinja from stripping it)
-capitalize --> capitalizes first word
-lower --> all lower case
-upper -->ALL CAPS
-title --> title case
-trim --> removes trailing spaces from the end
-striptags--> removes html tags
-'''
+   
+    
+   
 
 #create a route (using decorator)
 @app.route('/')
 def index():
     first_name = "Loki"
     stuff = "This is <b>bold</b> text "
-    favorite_pizza=["Mushrooms", "Hawaiian", "Perppers", "Cheese", 32]
+    favorite_pizza = ["Mushrooms", "Hawaiian","Cheese", "Peppers", 32]
     return render_template("index.html", 
                            first_name = first_name, 
                            stuff=stuff, 
@@ -30,7 +23,16 @@ def index():
 def user(name):
     return render_template("user.html", username=name) #"username" comes from func argument
 
-#NEXT: https://www.youtube.com/watch?v=4yaG-jFfePc&list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz&index=2
+#Custom error pages
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 #set environmental variables ( in terminal):
 #export FLASK_ENV=development, this is deprecated, try FLASK_DEBUG
