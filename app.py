eb1c7f01cff8eb2d1578939826216912d5f2f3c4
torcuata22 <marilynjmarquez@gmail.com>
2023-04-25 14:25:42 -0500

    this is hell

--- app.py
+++ app.py
@@ -5,7 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    first_name="Loki"
+    favorite_food="Tuna"
+    favorite_toys=["Stumpy", "my Bumble Bee", "Mousy", "The hallway rug"]
+    favorite_sound="Squeak!"
+
+    return render_template('index.html', first_name=first_name, favorite_food=favorite_food,favorite_toys=favorite_toys, favorite_sound=favorite_sound)
+
+@app.route('/user/<name>/')
+def user(name):
+    return render_template("user.html", name=name)
+
+#custom error pages
+@app.errorhandler(404)
+def page_not_founr(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_founr(e):
+    return render_template("500.html"), 500
+
+
 
 #in Terminal: 
 #export FLASK_ENV=development
