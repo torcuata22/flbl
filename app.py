f650681c1aec32cb48f9dab77565abb25e4c51e6
torcuata22 <marilynjmarquez@gmail.com>
2023-04-25 15:37:54 -0500

    db again

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
 
 #Create Flask Instance:
 app = Flask(__name__)
 
+#Secret key (csrf token for forms):
+app.config['SECRET_KEY']="this is my secret key"
+
+#Create Form Class:
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit =SubmitField("Submit")
+
+
+
+#Routes:
 @app.route('/')
 def index():
     first_name="Loki"
     favorite_food="Tuna"
     favorite_toys=["Stumpy", "my Bumble Bee", "Mousy", "The hallway rug"]
     favorite_sound="Squeak!"
-
     return render_template('index.html', first_name=first_name, favorite_food=favorite_food,favorite_toys=favorite_toys, favorite_sound=favorite_sound)
 
 @app.route('/user/<name>/')
 def user(name):
     return render_template("user.html", name=name)
+
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validation:
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+        flash('Form submitted successfully!')
+    return render_template('name.html', 
+                           name=name, 
+                           form=form,)
 
 #custom error pages
 @app.errorhandler(404)
